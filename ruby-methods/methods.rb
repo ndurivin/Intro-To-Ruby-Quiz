10ebab0bb2618b4baf053c7b1c7755f0004c69eb
Tonny Oseko <tonnyoseko0@gmail.com>
2022-08-26 17:03:37 +0300

    question 3

--- ruby-methods/methods.rb
+++ ruby-methods/methods.rb
@@ -1,4 +1,4 @@
-/# Ruby Methods
+# Ruby Methods
 ## Q1 Write any simple Ruby Program to illustrate the defining and calling of method
 
 ## Q2 You have a string, "I love espresso", return the length of the string
@@ -6,9 +6,16 @@
 ## Q3 Write a method where var1 and var2 are the parameters. Call the method. The statements to be executed are:
 #"Phase-1 was all about Introduction to Programming."
 #"Phase-2 was all about React."
+def method(var1, var2)
+    puts var1
+    puts var2
+end
+method("Phase-1 was all about Introduction to Programming.", "Phase-2 was all about React.")
 
 ##Q4 Write a Ruby program which accept the user's first and last name and print them in reverse order with a 
 #space between them.
+def input(firstname, lastname)
+    lastname
 
 ## Q5 Ruby program to illustrate the passing of parameters to a method
 # Output 
