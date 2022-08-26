1e4b0a8d2fe4c5ad37c40b2ba0cf7e798532452c
jackline chepngeno <chepngenojackline0@gmail.com>
2022-08-26 17:04:33 +0300

    app.rb

--- variables-and-data-types/app.rb
+++ variables-and-data-types/app.rb
@@ -2,10 +2,17 @@
 ## Q1 Set variables for the corresponding values:
 
 #a number to the value 25
+my_int = 25
+print my_int
 #a boolean to the value true 
+result = 5 < 8
+print result
 #a string to the value "Ruby"
+output = "Ruby"
+puts output
 
 ##  Q2 Set values 100, 30 for the  variables cars and drivers respectively.
 #Output: There are 100 cars and 30 drivers.
-
-
+ cars =100
+ drivers=30
+ puts "There are #{cars} cars and #{drivers} drivers"
